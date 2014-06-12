a4fb5f07cd4b421526f172685f70309f49e2afc2
Andrés Peñailillo <andres@penailillo.com>
2014-06-11 22:10:38 -0400

    Create The Love-Letter Mystery.rb

---
+++ Hacker Rank/Algorithms/The Love-Letter Mystery.rb
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/the-love-letter-mystery
+tc = gets.to_i
+tc.times{
+    arr = gets.chomp.split("").map(&:ord)
+    count = 0
+    start,finish = 0, arr.size-1
+    while start < finish
+        count += (arr[start]-arr[finish]).abs
+        start += 1
+        finish -= 1
+    end
+    p count
+}
